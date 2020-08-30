b585a24e9e4530a2dc24e15888c0e1570ea11921
Samuel Vieira <samuelvieirarpo@gmail.com>
2020-08-29 21:12:58 -0500

    Initial Commit

---
+++ EstadinatorSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EstadinatorSwift
+//
+//  Created by Samuel Vieira on 29/08/20.
+//  Copyright © 2020 Zoomers. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
